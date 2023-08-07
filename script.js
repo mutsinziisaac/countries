95483fc2488ece650d720b4a5431ce13f2b45804
Mutsinzi Isaac <mutsinziisaac123@gmail.com>
2023-08-07 17:38:02 +0300

    editing the style to have 4 coards each row

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
+let countriesData = []; // To store the fetched data
+
 // Fetch data from data.json
 fetch('data.json')
   .then((response) => response.json())
   .then((data) => {
-    // Process the data and create country cards
-    const countriesContainer = document.querySelector('.countries');
-
-    data.forEach((country) => {
-      const countryCard = createCountryCard(country);
-      countriesContainer.appendChild(countryCard);
-    });
+    countriesData = data;
+    updateCountryList(countriesData);
   })
   .catch((error) => console.error('Error fetching data:', error));
 
 // Helper function to create a country card
 function createCountryCard(countryData) {
+
   const countryCard = document.createElement('div');
   countryCard.classList.add('country-card');
 
@@ -31,7 +29,32 @@ function createCountryCard(countryData) {
   countryCard.appendChild(countryInfo);
 
   return countryCard;
-  
 }
 
+
+function updateCountryList(countries) {
+  const countriesContainer = document.querySelector('.countries');
+  countriesContainer.innerHTML = '';
+
+  countries.forEach((country) => {
+    const countryCard = createCountryCard(country);
+    countriesContainer.appendChild(countryCard);
+  });
+}
+
+// Function to filter countries based on the search input
+function searchCountries(searchTerm) {
+  const filteredCountries = countriesData.filter((country) => {
+    const countryName = country.name.toLowerCase();
+    return countryName.includes(searchTerm.toLowerCase());
+  });
+
+  updateCountryList(filteredCountries);
+}
+
+const searchInput = document.getElementById('searchInput');
+searchInput.addEventListener('input', (event) => {
+  const searchTerm = event.target.value;
+  searchCountries(searchTerm);
+});
 
